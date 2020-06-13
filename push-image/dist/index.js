875212485a143202a4d997e1183380e995bcdb1c
Andy Hume <andyhume@gmail.com>
2020-06-13 09:07:20 +0100

    feat: improve info log and command grouping (#5)

--- push-image/dist/index.js
+++ push-image/dist/index.js
@@ -2620,28 +2620,37 @@ async function run() {
   const repoTag = `eu.gcr.io/bw-prod-artifacts/${service}:${tag}`;
 
   try {
+    core.startGroup("Configuring Docker authentication");
     await exec.exec('gcloud', ['auth', 'configure-docker', 'eu.gcr.io', '--quiet'], {});
+    core.endGroup();
 
+    core.startGroup(`Building Docker image: ${repoTag}`);
     await push.buildImage({
         dockerFile: core.getInput('dockerFile'),
         contextPath: core.getInput('contextPath'),
         repoTag,
     })
+    core.endGroup();
 
+    core.startGroup(`Pushing Docker image: ${repoTag}`);
     await push.pushImage({
         repoTag,
     })
+    core.endGroup();
 
+    core.startGroup("Pushing Slipstream metadata");
     const data = await push.buildMetadata({
       event: require(process.env.GITHUB_EVENT_PATH),
       service: core.getInput('service'),
       repoTag,
       labels: core.getInput('labels'),
     })
-
     await push.pushMetadata(data);
+    core.endGroup();
 
-    core.setOutput('imageDigest', util.getImageDigest(data.dockerInspect))
+    core.setOutput('imageDigest', util.getImageDigest(data.dockerInspect));
+    core.info('success');
+    core.info(`Run 'slipstream list images -s ${service}' to view service images`);
 
   } catch ( error ) {
       core.setFailed(error.message);
