f0a094f70063e865ce6d6618a4af0a54f218ebf9
bw-bmbarga <bmbarga@brandwatch.com>
2021-05-17 14:43:33 +0200

    feat(push-images): add args to commandExists command
    We are adding the args parameter to the commandExists command
    so to prevent the `aws` command to fail when run without arguments.

--- deploy/dist/index.js
+++ deploy/dist/index.js
@@ -4600,8 +4600,8 @@ exports.removeRemoteTask = removeRemoteTask;
 
 const exec = __webpack_require__(663);
 
-async function commandExists(command) {
-  return exec.exec(command, [], { silent: true }).then(() => true).catch(() => false);
+async function commandExists(command, args) {
+  return exec.exec(command, args, { silent: true }).then(() => true).catch(() => false);
 }
 
 module.exports = commandExists;
@@ -6229,7 +6229,7 @@ async function installSlipstreamCLI(downloadURL) {
 }
 
 async function installAWSCLI() {
-  const commandExistsAlready = await commandExists('aws');
+  const commandExistsAlready = await commandExists('aws', ['--version']);
 
   if (commandExistsAlready) {
     return;
