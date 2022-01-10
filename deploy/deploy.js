7772b2429b0b574283815d0f395f2861642f525e
Oscar Molnar <tymscar@gmail.com>
2022-01-10 11:07:47 +0000

    chore: cleanup argument generation code

--- deploy/deploy.js
+++ deploy/deploy.js
@@ -1,26 +1,14 @@
 const exec = require('@actions/exec');
 
 async function deploy(environment, service, id, idKey) {
-  let args;
-  if (idKey === 'ID') {
-    args = [
-      'deploy',
-      environment,
-      service,
-      '--id', `${id}`,
-      '--quiet',
-      '--wait',
-    ];
-  } else {
-    args = [
-      'deploy',
-      environment,
-      service,
-      '--version', `${id}`,
-      '--quiet',
-      '--wait',
-    ];
-  }
+  const args = [
+    'deploy',
+    environment,
+    service,
+    `--${idKey.toLowerCase()}`, `${id}`,
+    '--quiet',
+    '--wait',
+  ];
 
   await exec.exec('slipstream', args, { silent: false });
 }
